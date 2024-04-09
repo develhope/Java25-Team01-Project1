edb1f5fe9eac9fe07fd86682824df29110b015dd
GiuseppeFavara97 <giuseppe.favara1997@gmail.com>
2024-04-09 16:44:40 +0200

    main migliorato

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,9 @@ public class Main {
         //richiamo del metodo risultato che riconosce l'operazione,
         // esegue l'operazione e stampa il risultato ottenuto.
 
-        System.out.println("Il risultato dell'operazone è " + Op.operazione('^',6,5));
+        System.out.println("Il risultato dell'operazone è ");
+        System.out.println( Op.operazione('-',6,5));
+
 
     }
 }
