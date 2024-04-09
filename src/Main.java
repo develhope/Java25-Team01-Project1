a2921a02c2da62abe78024b2d8f0dfac3a990af9
Ilaria <ilaria.faleschini@gmail.com>
2024-04-09 15:32:09 +0200

    Method exponential values

--- src/Main.java
+++ src/Main.java
@@ -20,6 +20,8 @@ public class Main {
     public static double divValues (double x, double y) {
         return x / y;
     }
+
+    //Method Even values
     public static String evenValues (double x, double y) {
         double sum = x + y;
         if (sum % 2 == 0) {
@@ -29,7 +31,8 @@ public class Main {
         }
     }
 
-    public static double exponentialValue (double x, double y) {
+    //Method exponential values
+    public static double exponentialValues (double x, double y) {
         double result = 1.0;
         for (int i=0; i < y; i++) {
             result *= x;
