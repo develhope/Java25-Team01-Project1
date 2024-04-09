9a0d084ff4e4dc7193e0ce0e95725b0ea0503202
Albano Giuseppe Persechino <163877689+AlbanoGiuseppePersechino@users.noreply.github.com>
2024-04-09 15:18:26 +0200

    Aggiunta medoto sottrazione, moltiplicazione e divisione

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,20 @@ public class Main {
 
     }
 
+    // Method of sum
     public static double sumValues (double x, double y) {
         return x + y;
     }
-
+    // Method of subtraction
+    public static double subValues (double x, double y) {
+        return x - y;
+    }
+    // Method of Multiplication
+    public static double MoltValues(double x , double y) {
+        return x * y;
+    }
+    // Method of Division
+    public static double divValues (double x, double y) {
+        return x / y;
+    }
 }
